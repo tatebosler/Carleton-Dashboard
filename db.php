71a5cf9415c2286590b94e8131e5c8ac38df7a0d
Tate Bosler <tatebosler@gmail.com>
2015-10-08 21:55:15 -0500

    Added empty db.php

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+  // FILL IN YOUR DATABASE INFORMATION HERE
+  
+  DB_HOST = "";
+  DB_USERNAME = "";
+  DB_PASSWORD = "";
+  DB_DATABASE = "";
+  
+  $db = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+?>
